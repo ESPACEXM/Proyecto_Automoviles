da8e6cc99103ab6f1eb94b7d99a040e41c3f868e
José Posadas <posadasjosep8@gmail.com>
2022-11-21 10:47:22 -0600

    Version Proyecto

--- view/Propietario/addmarcaView.php
+++ view/Propietario/addmarcaView.php
@@ -6,132 +6,40 @@ if (!$_SESSION['user_id']) {
 
 include_once("../../model/functions.php");
 
-
+$usrClass = new usuariosModel();
 
 $result = array();
 $resultRoles = array();
-
+$result = $usrClass->getUsuarios();
 
 ?>
 <script src="assets/js/moduloUsuarios.js"></script>
 <link rel="stylesheet" href="../../assets/css/temas.css">
-<div class="card">
-    <div class="card-header">
-        <div
-            class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 class="h2">LISTADO DE MARCAS</h1>
-        </div>
-    </div>
+<body>
 
-    <div class="card-body">
+    <section class="form_wrap">
 
+        <section class="cantact_info">
+            <section class="info_title">
+                <span class="fa fa-user-circle"></span>
+                <h2>AGREGAR<br>MARCA</h2>
+            </section>
+            
+        </section>
 
-        <div class="container">
-            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                <button class="btn btn-success me-md-2" id="btnNuevoUsuario" name="btnNuevoUsuario" type="button"
-                    data-bs-toggle="modal" data-bs-target="#formNuevoUsuario">Nueva Marca</button>
+        <form action="" class="form_contact">
+            <h2>Ingresar datos</h2>
+            <div class="user_info">
+                <label for="names">Nombre</label>
+                <input type="text" id="names" required="">
+
+                <label for="names">Estado</label>
+                <input type="text" id="phone">
+
+                <button class="button-guardar">Guardar</button>
             </div>
-            <div class="table-responsive">
-                <table class="table">
-                    <thead>
-                        <tr>
-                        <th scope="col">ID</th>
-                            
-                            <th scope="col">MARCA</th>
-                            <th scope="col">EDITAR</th>
-                            <th scope="col">ELEMINAR</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+        </form>
 
-                        <?php 
-                while ($fila = mysqli_fetch_array($result)){
-                    ?>
-                         <tr>
-                       
-                            <td><?php echo $fila['marca']; ?></td>
-                           
-                            <td>
-                                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                                    <button class="btn btn-warning me-md-2" id="btnEditarAutomovil"
-                                        name="btnEditarAutomovil" type="button" onclick="obtenerAutomovil(<?php echo $fila['id']; ?>);">Editar</button>
-                                </div>
-                            </td>
-                            <td>
-                                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                                    <button class="btn btn-danger me-md-2" id="btnEliminarAutomovil"
-                                        onclick="eliminarAutomovil(<?php echo $fila['id']; ?>);" name="btnEliminarAutomovil"
-                                        type="button">Eliminar</button>
-                                </div>
-                            </td>
-                        </tr>
+    </section>
 
-                    <?php 
-                    }
-                    ?>
-
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-    <!-- AQUI INICIA ESTA EL FORMULARIO MODAL PARA AGREGAR USUARIOS -->
-    <div class="modal fade" id="formNuevoUsuario" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-        aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-
-                <div class="modal-header">
-                    <h5 class="modal-title" id="formNuevoUsuario">Nueva Marca</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-
-                <div class="modal-body">
-                    <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="nombres" placeholder="aqui va tu nombre">
-                        <label for="nombres">Agregar Marca</label>
-                    </div>
-                   
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-primary" id="btnAgregarUsuario">Agregar Marca</button>
-                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
-                </div>
-
-            </div>
-        </div>
-    </div>
-
-    <!-- AQUI INICIA ESTA EL FORMULARIO MODAL PARA AGREGAR USUARIOS -->
-    <div class="modal fade" id="formActualizaUsuario" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-        aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-
-                <div class="modal-header">
-                    <h5 class="modal-title" id="formActualizaUsuario">Agregar Marca</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-
-                <div class="modal-body">
-
-                    <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="id_upd">
-                        <label for="id_upd">ID</label>
-                    </div>
-
-                    <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="nombres_upd" placeholder="aqui va tu nombre">
-                        <label for="nombres_upd">Agregar Marca</label>
-                    </div>
-
-
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-primary" id="btnActualizarUsuario">Actualizar Marca</button>
-                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
-                </div>
-
-            </div>
-        </div>
-    </div>
+</body>
